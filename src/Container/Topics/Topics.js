6dc76b1d8dd992e06e457f598fdc9c1b8232ebcf
alapatbh <44811890+alapatbh@users.noreply.github.com>
2020-04-25 17:12:52 -0400

    Added topics data to store reduce

--- src/Container/Topics/Topics.js
+++ src/Container/Topics/Topics.js
@@ -6,30 +6,60 @@ import "./Topics.css";
 class Topics extends Component {
   state = {
     listOfTopicsByChapter: [],
+    newTopic: "",
   };
 
   componentDidMount() {
     // console.log("[Topics.js] component did mount");
-    const newTopisList = ["Serialization", "externalization", "Topics"];
+    const newTopisList = [{ topicId: 1, chapterId: 1, topicName: "Strings" }];
     this.setState({ listOfTopicsByChapter: newTopisList });
   }
 
+  onTopicChangeHandler = (event) => {
+    this.setState({ newTopic: event.target.value });
+  };
+
+  onNewTopicAddButton = () => {
+    const newTopicObj = {
+      topicId: Math.random(),
+      chapterId: this.props.storedChapter.chapterId,
+      topicName: this.state.newTopic,
+    };
+    let addedTopicToList = [...this.state.listOfTopicsByChapter, newTopicObj];
+    this.setState({ listOfTopicsByChapter: addedTopicToList, newTopic: "" });
+  };
+
   render() {
     let topicsBody = <div></div>;
-    // console.log(this.state.listOfTopicsByChapter);
-    const topicList = this.state.listOfTopicsByChapter.map(
-      (eachTopic, index) => (
-        <div className="eachChapter" key={index}>
-          {eachTopic}
-        </div>
-      )
-    );
+
     if (this.props.storedChapter) {
+      const topicListFilter = this.state.listOfTopicsByChapter.filter(
+        (eachTopic) =>
+          eachTopic.chapterId === this.props.storedChapter.chapterId
+      );
+      const topicList = topicListFilter.map((eachTopic, index) => (
+        <div
+          className="eachChapter"
+          key={index}
+          onClick={() => this.props.onClickTopic(eachTopic)}
+        >
+          {eachTopic.topicName}
+        </div>
+      ));
       topicsBody = (
         <div className="TopicsCss">
           <b>Topics in {this.props.storedChapter.chapterName}</b>
-
           <div className="innerTopicsDiv">{topicList}</div>
+          <div>
+            <input
+              type="text"
+              size="10"
+              value={this.state.newTopic}
+              onChange={this.onTopicChangeHandler}
+            />
+            &nbsp;
+            <button onClick={this.onNewTopicAddButton}>+</button>
+          </div>
         </div>
       );
     }
@@ -41,4 +71,13 @@ const mapStateToProps = (state) => {
   return { storedChapter: state.chapter };
 };
 
-export default connect(mapStateToProps)(Topics);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onClickTopic: (topic) =>
+      dispatch({
+        type: "SELECTEDTOPIC",
+        value: topic,
+      }),
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Topics);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,15 +1,18 @@
 const intialState = {
   course: null,
   chapter: null,
+  topic: null,
 };
 
 const reducer = (state = intialState, action) => {
   //console.log(state);
   switch (action.type) {
     case "SELECTEDCOURSE":
-      return { ...state, course: action.value, chapter: null };
+      return { ...state, course: action.value, chapter: null, topic: null };
     case "SELECTEDCHAPTER":
-      return { ...state, chapter: action.value };
+      return { ...state, chapter: action.value, topic: null };
+    case "SELECTEDTOPIC":
+      return { ...state, topic: action.value };
     default:
       return state;
   }
