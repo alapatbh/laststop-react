e0dcae3059de73bc3ed6e92ed33c0182c25eb227
alapatbh <44811890+alapatbh@users.noreply.github.com>
2020-05-03 01:03:42 -0400

    Dynamic addition of Question and answer.

--- src/Container/QuestionAndAnswer/QuestionAndAnswer.js
+++ src/Container/QuestionAndAnswer/QuestionAndAnswer.js
@@ -7,6 +7,37 @@ import Auxiliary from "../../Hoc/Auxiliary";
 class QuestionAndAnswer extends Component {
   state = {
     listQuesAndAns: [],
+    inputQuestion: "",
+    inputAnswer: "",
+    inputDifficulty: "",
+  };
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  addQuestionHandler = () => {
+    console.log("[QuestionAnsAnswer.js] -> addQuestionHandler");
+    const newQuesAndAns = {
+      questionId: Math.random(),
+      question: this.state.inputQuestion,
+      answer: this.state.inputAnswer,
+      difficulty: this.state.difficulty,
+      topicId: this.props.storedTopic.topicId,
+    };
+    const changedList = [...this.state.listQuesAndAns, newQuesAndAns];
+    console.log("[QuestionAnsAnswer.js] -> " + changedList);
+    this.setState({
+      listQuesAndAns: changedList,
+      inputQuestion: "",
+      inputAnswer: "",
+    });
   };
 
   componentDidMount() {
@@ -19,13 +50,6 @@ class QuestionAndAnswer extends Component {
         difficulty: "h",
         topicId: 1,
       },
-      {
-        questionId: 2,
-        question: "What are different ways to create String Object?",
-        answer: 'String str = new String("abc");    String str1 = "abc";',
-        difficulty: "m",
-        topicId: 1,
-      },
     ];
     this.setState({ listQuesAndAns: QuestionAndAnswerList });
   }
@@ -36,7 +60,7 @@ class QuestionAndAnswer extends Component {
       const questionsRelatedToTopic = this.state.listQuesAndAns.filter(
         (eachQues) => eachQues.topicId === this.props.storedTopic.topicId
       );
-      const quesMap = questionsRelatedToTopic.map((eachQues, index) => {
+      const quesMap = questionsRelatedToTopic.map((eachQues) => {
         return (
           <Auxiliary key={eachQues.questionId}>
             <EachQuesAndAns
@@ -47,10 +71,52 @@ class QuestionAndAnswer extends Component {
           </Auxiliary>
         );
       });
+
       QuesAndAnsBody = (
         <div className="QuesAndAnsCss">
           <div className="quesHeading">
             <b>Questions in {this.props.storedTopic.topicName}</b>
+          </div>
+          <div>
+            <table className="quesAnsTable">
+              <tbody>
+                <tr>
+                  <td className="headingTd">Question:</td>
+                  <td className="mainTd">
+                    <input
+                      onChange={(e) => this.handleInputChange(e)}
+                      name="inputQuestion"
+                      value={this.state.inputQuestion}
+                      type="text"
+                    />
+                  </td>
+                  <td className="rightTd">
+                    <select
+                      onChange={(e) => this.handleInputChange(e)}
+                      name="inputDifficulty"
+                    >
+                      <option value="e">Easy</option>
+                      <option value="m">Medium</option>
+                      <option value="h">Hard</option>
+                    </select>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Answer:</td>
+                  <td>
+                    <textarea
+                      onChange={(e) => this.handleInputChange(e)}
+                      value={this.state.inputAnswer}
+                      name="inputAnswer"
+                      rows="3"
+                    />
+                  </td>
+                  <td>
+                    <button onClick={this.addQuestionHandler}>Save</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
           <div className="QuesMap">{quesMap}</div>
         </div>
